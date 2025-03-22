26494d223d5d3ea4eded9782a61db4b7ebf5573c
ChidoubReda <Reda.Chidoub@emsi-edu.ma>
2025-03-22 15:00:13 +0100

    First Commit

---
+++ src/main/java/com/springdemo/orm_jpa_hibernate_springdata/OrmJpaHibernateSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.orm_jpa_hibernate_springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmJpaHibernateSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmJpaHibernateSpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springdemo/orm_jpa_hibernate_springdata/OrmJpaHibernateSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.orm_jpa_hibernate_springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmJpaHibernateSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
